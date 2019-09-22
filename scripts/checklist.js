ae987eb944557e4afc9f7e5087789cee2ee4b00f
Jason Jose <jasonsisonjose@csu.fullerton.edu>
2019-09-21 21:06:56 -0700

    finished chapter 11

---
+++ scripts/checklist.js
@@ -0,0 +1,72 @@
+(function (window) {
+  'use strict';
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function CheckList(selector) {
+    if (!selector) {
+      throw new Error("no selector passed in");
+    }
+
+    this.$element = $(selector);
+    if(this.$element.length === 0) {
+      throw new Error("Cannot find selector element using: " + selector);
+    }
+  }
+
+  CheckList.prototype.addRow = function (coffeeOrder) {
+    this.removeRow(coffeeOrder.emailAddress);
+    var rowElement =new Row(coffeeOrder);
+    this.$element.append(rowElement.$element);
+  };
+
+  CheckList.prototype.removeRow = function (email) {
+    this.$element
+      .find('[value="' + email + '"]')
+      .closest('[data-coffee-order="checkbox"]')
+      .remove();
+  };
+
+  CheckList.prototype.addClickHandler = function (fn) {
+    this.$element.on('click', 'input',function(event) {
+      var email = event.target.value;
+      this.removeRow(email);
+      fn(email);
+    }.bind(this));
+  };
+
+  function Row(coffeeOrder) {
+    // $div is not an instance variable, it is a reference
+    // to DOM element created by jQuery
+    var $div = $('<div></div>', {
+      'data-coffee-order' : 'checkbox',
+      'class': 'checkbox'
+    });
+
+    var $label = $('<label></label>');
+
+    var $checkbox = $('<input></input', {
+      type: 'checkbox',
+      value: coffeeOrder.emailAddress
+    });
+
+    var description = coffeeOrder.size + ' ';
+    if (coffeeOrder.flavor) {
+      description += coffeeOrder.flavor + ' ';
+    }
+    description += coffeeOrder.coffee + ', ';
+    description += ' (' + coffeeOrder.emailAddress + ') ';
+    description += ' [' + coffeeOrder.strength + 'x]';
+
+    $label.append($checkbox);
+    $label.append(description);
+    $div.append($label);
+
+    this.$element = $div;
+  }
+
+
+
+  App.CheckList = CheckList;
+  window.App = App;
+})(window);
