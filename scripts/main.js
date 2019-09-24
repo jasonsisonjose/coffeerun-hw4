ff3d3793d0bf8ce1a81c8e73cc764ea6feaf3b63
Jason Jose <jasonsisonjose@csu.fullerton.edu>
2019-09-24 16:14:54 -0700

    working on jQuery

--- scripts/main.js
+++ scripts/main.js
@@ -2,23 +2,28 @@
   'use strict';
   var FORM_SELECTOR = '[data-coffee-order="form"]';
   var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+  var PAYMENT_SELECTOR = '[data-coffee-order="payment-form"]';
+
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
-  var CheckList = App.CheckList;
+//  var CheckList = App.CheckList;
 
   var myTruck = new Truck('Serenity', new DataStore());
   window.myTruck = myTruck;
 
-  var checkList = new CheckList(CHECKLIST_SELECTOR);
-  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+//  var checkList = new CheckList(CHECKLIST_SELECTOR);
+//  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
 
-  var formHandler = new FormHandler(FORM_SELECTOR);
+//  var formHandler = new FormHandler(FORM_SELECTOR);
+  var paymentHandler = new FormHandler(PAYMENT_SELECTOR);
 
-  formHandler.addSubmitHandler(function (data) {
-    myTruck.createOrder.call(myTruck,data);
-    checkList.addRow.call(checkList, data);
-  });
-  console.log(formHandler);
+  paymentHandler.addSubmitHandler();
+
+  // formHandler.addSubmitHandler(function (data) {
+  //   myTruck.createOrder.call(myTruck,data);
+  //   checkList.addRow.call(checkList, data);
+  // });
+//  console.log(formHandler);
 })(window);
